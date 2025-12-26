3d2fb407aa58b0c81caa9f424c367dc9acba5dd9
CoyD1 <apranovich22010kostya@mail.ru>
2025-12-27 01:28:09 +0700

    first commit

---
+++ Color.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <istream>
+
+class Color {
+  public:
+    Color();
+    Color(double red, double green, double blue);
+    double red() const;
+    double green() const;
+    double blue() const;
+    inline friend std::istream& operator>>(std::istream& stream, Color& color) {
+        return stream >> color.r >> color.g >> color.b;
+    }
+
+  private:
+    double r{};
+    double g{};
+    double b{};
+};
